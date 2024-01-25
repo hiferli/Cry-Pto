01a59fd09e11a013c13473cbf26f7a8981b5a44a
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-01-25 20:19:15 +0530

    Fetching API and Showing Data

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 import LineGraph from './Components/Charts/LineGraph'
+import axios from 'axios'
 
 function App() {
-	const [xData, setXData] = useState([1]);
-	const [yData, setYData] = useState([2]);
-	
+	const [xData, setXData] = useState([0]);
+	const [yData, setYData] = useState([0]);
+
+	const getData = async () => {
+		await axios.get(
+			'http://localhost:3000/api/data',
+			{
+				params: {
+					min: 1,
+					max: 100,
+					prev: ((xData.length) ? xData[xData.length - 1] : 0)
+				}
+			}
+		).then((response) => {
+			const values = response.data.coordinates;
+			setXData([...xData , values.x])
+			setYData([...yData , values.y])
+		})
+		.catch(error => console.log(error))
+	};
+
+	const addValue = () => {
+		getData()
+	}
+
 	return (
 		<>
 			<h1>this is a line chart</h1>
