034b1c457960d90ef08a486b206a694b652f0555
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-01-25 20:19:04 +0530

    Modifying API

--- Server/Controllers/GetCustomData.js
+++ Server/Controllers/GetCustomData.js
@@ -1,13 +1,16 @@
 
 export const getCustomData = (request , response) => {
-    const { min = 100 , max = 10 } = request.body;
+    let min = parseFloat(request.query.min) || 10;
+    let max = parseFloat(request.query.max) || 100;
+    let prev = parseFloat(request.query.prev) || 0;
 
+    console.log({min , max , prev})
     const y = +((Math.random() * (max - min) + min).toFixed(2));
-    const x = +((Math.random() * (max - min) + min).toFixed(2));
+    const x = prev + 1;
 
     return response.status(200).json({
         status: true,
-        data: {
+        coordinates: {
             x: x,
             y: y
         }
