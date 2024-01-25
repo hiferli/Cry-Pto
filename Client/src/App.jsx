e8e5b6eac29bb4116ef4169affe37c8ab771ca8b
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-01-25 21:03:04 +0530

    Adding Metrics

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import LineGraph from './Components/Charts/LineGraph'
 import axios from 'axios'
@@ -6,14 +6,15 @@ import axios from 'axios'
 function App() {
 	const [xData, setXData] = useState([0]);
 	const [yData, setYData] = useState([0]);
+	const [metrics, setMetrics] = useState({});
 
 	const getData = async () => {
 		await axios.get(
 			'http://localhost:3000/api/data',
 			{
 				params: {
-					min: 1,
-					max: 100,
+					min: (metrics.min || 1),
+					max: (metrics.max || 100),
 					prev: ((xData.length) ? xData[xData.length - 1] : 0)
 				}
 			}
@@ -25,15 +26,36 @@ function App() {
 		.catch(error => console.log(error))
 	};
 
-	const addValue = () => {
-		getData()
+	const updateMetrics = (event) => {
+		event.preventDefault();
+		setMetrics({...metrics , [event.target.name]: event.target.value})
 	}
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			getData();
+		}, ((metrics.freq * 1000) || 1000));
+
+		return () => clearInterval(interval);
+	}, [xData])
+	
 
 	return (
 		<>
-			<h1>this is a line chart</h1>
+			<h1>Graph</h1>
+			
+			<div>
+				<label htmlFor="min">Min Value:</label>
+				<input onChange={updateMetrics} type="number" name='min' id='min' min={1}  />
+					<br />
+				<label htmlFor="max">Max Value:</label>
+				<input onChange={updateMetrics} type="number" name='max' id='max' min={1}  />
+					<br />
+				<label htmlFor="freq">Frequency:</label>
+				<input onChange={updateMetrics} type="number" name='freq' id='freq' min={1} />
+			</div>
+
 			<LineGraph xData={xData} yData={yData} />
-			<button onClick={addValue}>Add</button>
 		</>
 	)
 }
